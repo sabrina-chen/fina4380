51a37d169770292a5fb612e6aaf7875dd3d7ea4e
Sabrina <sabrinaypchen@gmail.com>
2021-04-10 18:01:57 +0800

    Update performance evaluation. To be completed

--- Performance.py
+++ Performance.py
@@ -1,14 +1,32 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Performance:
     def __init__(self, NAV):
         self.nav = np.array(NAV).reshape(-1)
     
     def analyze(self, rf):
-        self.cr = self.nav[-1] / self.nav[0] - 1
-        self.ar = (self.nav[-1] / self.nav[0]) ^ (250/len(self.nav)) - 1
-        self.ann_vol = self.nav.std() * 16
-        self.asr = (self.ar - rf) / self.ann_vol
-        
+        cr = self.nav[-1] / self.nav[0] - 1
+        ar = (self.nav[-1] / self.nav[0]) ^ (250/len(self.nav)) - 1
+        ann_vol = self.nav.std() * 16
+        asr = (ar - rf) / ann_vol
+        drawdown = []
+        for i in len(self.nav):
+            a = (self.nav[i]/self.nav[0:i].max()) - 1
+            drawdown.append(a)
+        md = drawdown.min()
+        calmer_ratio = ar/md
+        print("Cumulative return: " + cr,
+              "Annualized goemetric return: " + ar,
+              "Annualized std. : " + ann_vol,
+              "Annualized Sharpe Ratio: " + asr,
+              "Max drawdown: " + md,
+              "Calmer ratio: " + calmer_ratio,
+              sep="\n")
+
+    def plotting(self):
+        plt.plot(self.nav)
+        plt.show()
+
         

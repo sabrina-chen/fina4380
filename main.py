71f6eb6e0d07f7b09d9ab1b6949b798289422fae
Andy Lo <andyluoyuan@gmail.com>
2021-04-16 15:05:27 +0800

    main fixed

--- main.py
+++ main.py
@@ -2,49 +2,56 @@ import pandas as pd
 import numpy as np
 from DailyUpdate import DailyUpdate
 from Portfolio import Portfolio
-from Performance import Performance
 import datetime
+from matplotlib import pyplot as plt
+from Performance import Performance
 
 
-tradeTickers = pd.read_csv("./tradeTickers_filtered.csv", index_col=0)
-tradeTickers
-priceUse = pd.read_csv("./PriceUse_filtered.csv", index_col=0)
-priceUse
-priceUse.index = pd.to_datetime(priceUse.index)
+startyear = 2019
+nindustry = 20
 
-# cumret = (np.log(priceUse.iloc[:250,:]) - np.log(priceUse.iloc[0,:]))
-# cumret
+tradeTickers = pd.read_csv(f"./tradeTickers_filtered_{nindustry}industries.csv", index_col=0)
+# tradeTickers
+priceClose = pd.read_csv("./PriceUse_filtered.csv", index_col=0)
+priceOpen = pd.read_csv("./PriceUse_Open.csv", index_col=0)
+priceClose.index = pd.to_datetime(priceClose.index)
+priceOpen.index = pd.to_datetime(priceOpen.index)
 
-# todaytrade = DailyUpdate(cumret, tradeTickers)
-# todaytrade.rebalancing()
-# newport = todaytrade.getUpdatedPort()
-# newport.to_csv("./testport.csv")
-# todaytrade.newPort = pd.read_csv("./testport.csv", index_col=0)
 
-start = priceUse.index.searchsorted(datetime.datetime(2016, 1, 1))
+start = priceClose.index.searchsorted(datetime.datetime(startyear, 1, 1))
+
+indusList = pd.read_csv(f"./industryList_{nindustry}.csv", index_col=0)
+indusList.columns = [0]
+
+portlog = pd.DataFrame(np.array([[i]*2 for i in indusList[0]]).reshape(-1))
+
+gNAVlog = pd.DataFrame([], index=indusList[0])
+gNAVlog.index.name = None
+
+
 k = 0
-tmp = pd.read_csv("./newport.csv", index_col=0)
-portlog = tmp["1"].to_frame()
-portlog.columns = [0]
-
-k = 0
-for day in range(30):
-    cumret = (np.log(priceUse.iloc[start+day-250:start+day,:]) - np.log(priceUse.iloc[start+day-250,:]))
+for day in range(250):    # 250 for 1 year
+    print(f"day {k}")
+    # cumret = (np.log(priceClose.iloc[start+day-250:start+day,:]) - np.log(priceClose.iloc[start-250,:]))
+    
     if day == 0:
-        todayPortUpdate = DailyUpdate(cumret, tradeTickers)
+        todayPortUpdate = DailyUpdate(priceClose.iloc[start-250:start+day,:], tradeTickers)
     else:
-        todayPortUpdate = DailyUpdate(cumret, tradeTickers, newport, startEpsilon)
+        todayPortUpdate = DailyUpdate(priceClose.iloc[start-250:start+day,:], tradeTickers, newport, startEpsilon, startDay)
     todayPortUpdate.rebalancing()
     newport = todayPortUpdate.getUpdatedPort().copy()
     newport.columns = [0, 1, 2]
     startEpsilon = todayPortUpdate.startEpsilon.copy()
+    startDay = todayPortUpdate.startDay.copy()
     portlog = portlog.join(pd.DataFrame(newport.drop([1], axis=1).to_numpy(), columns=[2*k+1, 2*k+2]))
     portlog.to_csv("./portlog.csv")
     if k == 0:
-        tradePortfolio = Portfolio(tradeTickers, 10000000, priceUse.iloc[start+day,:].to_frame(), newport.copy(), priceUse.index[start], 2)
+        tradePortfolio = Portfolio(tradeTickers, 10000000, priceOpen.iloc[start,:].to_frame(), newport.copy(), priceOpen.index[start], 1)
     else:
-        tradePortfolio.updatePortfolio(priceUse.iloc[start+day,:].to_frame().T, newport.copy())
+        tradePortfolio.updatePortfolio(priceOpen.iloc[start+day,:].to_frame().T, newport.copy())
     tradePortfolio.NAVlog.to_csv("./NAVlog.csv")
+    gNAVlog = gNAVlog.join(pd.DataFrame(tradePortfolio.groupNAV.to_numpy(), columns=[k], index=gNAVlog.index))
+    gNAVlog.to_csv("./groupNAVlog.csv")
     k += 1
 
 
@@ -53,44 +60,4 @@ NAVlog = pd.read_csv("./NAVlog.csv", index_col=True)
 portfolio = Performance(NAV=NAVlog)
 Performance.analyze(self=portfolio, rf=0.017)
 Performance.plotting(self=portfolio, symbol='sp500')
-
-
-##### for debug
-# todayPort = todaytrade.getUpdatedPort()
-# todayPort.columns = [0, 1, 2]
-
-# port = Portfolio(tradeTickers, 10000000, priceUse.iloc[start,:].to_frame(), todayPort, priceUse.index[start], 2)
-# port.updatePortfolio(priceUse.iloc[start+1,:].to_frame().T, todayPort)
-# port.NAVlog
-# port.cash
-# port.proceed
-# port.portweight.head()
-# port.shares.head()
-# port.NAV
-
-# port.lev
-# port.margin
-# port.borrCash*2
-# port.values[port.values[2] < 0][2]
-# port.values[port.values[2] > 0][2]
-# port.values[2].sum() + port.margin.sum() + port.cash #+ port.proceed.sum() - port.borrCash.sum()
-# port.values[port.values[2] > 0][2].sum() + port.margin.sum() + port.proceed.sum() - abs(port.values[port.values[2] < 0][2].sum()) - port.borrCash.sum()
-
-
-# newport = todayPort.copy()
-# newport.iloc[0,0] = "F"
-# newport.iloc[1,0] = "THO"
-# newport.iloc[1,2] = 1.5
-
-# port.updatePortfolio(priceUse.iloc[start+3,:].to_frame().T, newport)
-
-
-# tvalue, epsilon = todaytrade.findPair("Banks")    
-# epsilon.sort_values([2])
-# tvalue.to_csv("./tvalue.csv")
-# todaytrade.getUpdatedPort()
-#####
-
-
-
 
